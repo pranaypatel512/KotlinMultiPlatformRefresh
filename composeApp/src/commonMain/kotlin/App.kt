56ad93c375f52fcca27324e85612e485c5f95893
pranaypatel512 <www.pranay1337.com@gmail.com>
2024-08-02 15:55:47 +0530

    updated with location

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,11 +1,16 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,7 +23,9 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 import kotlinmultiplatformrefresh.composeapp.generated.resources.Res
 import kotlinmultiplatformrefresh.composeapp.generated.resources.compose_multiplatform
 import kotlinx.datetime.Clock
+import kotlinx.datetime.IllegalTimeZoneException
 import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.LocalTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 
@@ -27,16 +34,21 @@ import kotlinx.datetime.toLocalDateTime
 fun App() {
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
+        var timeAtLocation by remember { mutableStateOf("No Location selected") }
+        var location by remember { mutableStateOf("Europe/Paris") }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             Button(onClick = { showContent = !showContent }) {
                 Text("Click me!")
             }
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                Column(
+                    Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
                     Text(
                         text = "Today's date is ${todaysDate()}",
-                                modifier = Modifier.padding(20.dp),
+                        modifier = Modifier.padding(20.dp),
                         fontSize = 24.sp,
                         textAlign = TextAlign.Center
                     )
@@ -44,13 +56,34 @@ fun App() {
                     Text("Compose: $greeting")
                 }
             }
+            Divider(modifier = Modifier.height(2.dp))
+            Spacer(modifier = Modifier.fillMaxWidth())
+            Text(timeAtLocation)
+            TextField(value = location, onValueChange = {
+                location = it
+            })
+            Button(onClick = { timeAtLocation = currentTimeAt(location) ?: "Invalid Location" }) {
+                Text("Show time at Location")
+            }
         }
     }
 }
 
-fun todaysDate():String{
+fun todaysDate(): String {
     fun LocalDateTime.format() = toString().substringBefore("T")
     val now = Clock.System.now()
     val zone = TimeZone.currentSystemDefault()
     return now.toLocalDateTime(zone).format()
+}
+
+fun currentTimeAt(location: String): String? {
+    fun LocalTime.formatted() = "$hour:$minute:$second"
+    return try {
+        val time = Clock.System.now()
+        val zone = TimeZone.of(location)
+        val localTime = time.toLocalDateTime(zone).time
+        "The time in $location is ${localTime.formatted()}"
+    } catch (ex: IllegalTimeZoneException) {
+        null
+    }
 }
